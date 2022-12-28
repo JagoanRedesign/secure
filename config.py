93437c6eb46bd7c1526c8e4d5f788bd32d38a282
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2022-12-28 16:33:18 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", ""))
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+STRING_SESSION6 = getenv("STRING_SESSION6", "")
+STRING_SESSION7 = getenv("STRING_SESSION7", "")
+STRING_SESSION8 = getenv("STRING_SESSION8", "")
+STRING_SESSION9 = getenv("STRING_SESSION9", "")
+STRING_SESSION10 = getenv("STRING_SESSION10", "")
