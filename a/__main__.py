68d12911d3617ea1b9b3a2e5205638ce91c19cbd
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2022-12-29 01:24:59 +0700

    Update __main__.py

--- a/__main__.py
+++ a/__main__.py
@@ -1,14 +1,14 @@
 from pyrogram import idle
 from uvloop import install
 from a import bots, LOOP, aiosession
-
+LER = -1001879930806
 async def main():
     for bot in bots:
         try:
             await bot.start()
             bot.me = await bot.get_me()
             await bot.join_chat("siapataudia")
-            await bot.send_message(LOG, "aktif")
+            await bot.send_message(LER, "aktif")
         except Exception as a:
             print(f"**ERROR:** `{str(a)}`")
     await idle()
