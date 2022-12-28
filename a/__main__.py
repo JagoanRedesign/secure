9703aadf77e4ed53420f9d6e94323469c1bb1834
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2022-12-28 16:22:12 +0700

    Create __main__.py

---
+++ a/__main__.py
@@ -0,0 +1,19 @@
+from pyrogram import idle
+from uvloop import install
+from a import bots, LOOP
+
+async def main():
+    for bot in bots:
+        try:
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("siapataudia")
+        except Exception as a:
+            LOGGER("main").warning(a)
+    await idle()
+    await aiosession.close()
+
+
+if __name__ == "__main__":
+    install()
+    LOOP.run_until_complete(main())
