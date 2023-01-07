285d6f70b7c527a73417fe8a6ac4715a8857b3c9
khansa gunawan <112273130+failedc0de@users.noreply.github.com>
2023-01-07 12:16:15 +0700

    Update __main__.py

--- a/__main__.py
+++ a/__main__.py
@@ -1,20 +1,209 @@
+import asyncio
 from pyrogram import idle
-from uvloop import install
-from a import bots, LOOP, aiosession
-LER = -1001879930806
-async def main():
-    for bot in bots:
-        try:
-            await bot.start()
-            bot.me = await bot.get_me()
-            await bot.join_chat("siapataudia")
-            await bot.send_message(LER, "aktif")
-        except Exception as a:
-            print(f"**ERROR:** `{str(a)}`")
-    await idle()
-    await aiosession.close()
+from . import 
 
+if bot1:
+    bot1.start()
+    bot1.join_chat("spambotte")
+   
+if bot2:
+    bot2.start()
+    bot2.join_chat("spambotte")
+   
+if bot3:
+    bot3.start()
+    bot3.join_chat("spambotte")
+   
+if bot4:
+    bot4.start()
+    bot4.join_chat("spambotte")
+   
+if bot5:
+    bot5.start()
+    bot5.join_chat("spambotte")
+   
+if bot6:
+    bot6.start()
+    bot6.join_chat("spambotte")
+   
+if bot7:
+    bot7.start()
+    bot7.join_chat("spambotte")
+   
+if bot8:
+    bot8.start()
+    bot8.join_chat("spambotte")
+   
+if bot9:
+    bot9.start()
+    bot9.join_chat("spambotte")
+   
+if bot10:
+    bot10.start()
+    bot10.join_chat("spambotte")
+   
 
-if __name__ == "__main__":
-    install()
-    LOOP.run_until_complete(main())
+if bot11:
+    bot11.start()
+    bot11.join_chat("spambotte")
+   
+if bot12:
+    bot12.start()
+    bot12.join_chat("spambotte")
+   
+if bot13:
+    bot13.start()
+    bot13.join_chat("spambotte")
+   
+if bot14:
+    bot14.start()
+    bot14.join_chat("spambotte")
+   
+if bot15:
+    bot15.start()
+    bot15.join_chat("spambotte")
+   
+if bot16:
+    bot16.start()
+    bot16.join_chat("spambotte")
+   
+if bot17:
+    bot17.start()
+    bot17.join_chat("spambotte")
+   
+if bot18:
+    bot18.start()
+    bot18.join_chat("spambotte")
+   
+if bot19:
+    bot19.start()
+    bot19.join_chat("spambotte")
+   
+if bot20:
+    bot20.start()
+    bot20.join_chat("spambotte")
+   
+
+if bot21:
+    bot21.start()
+    bot21.join_chat("spambotte")
+   
+if bot22:
+    bot22.start()
+    bot22.join_chat("spambotte")
+   
+if bot23:
+    bot23.start()
+    bot23.join_chat("spambotte")
+   
+if bot24:
+    bot24.start()
+    bot24.join_chat("spambotte")
+   
+if bot25:
+    bot25.start()
+    bot25.join_chat("spambotte")
+   
+if bot26:
+    bot26.start()
+    bot26.join_chat("spambotte")
+   
+if bot27:
+    bot27.start()
+    bot27.join_chat("spambotte")
+   
+if bot28:
+    bot28.start()
+    bot28.join_chat("spambotte")
+   
+if bot29:
+    bot29.start()
+    bot29.join_chat("spambotte")
+   
+if bot30:
+    bot30.start()
+    bot30.join_chat("spambotte")
+   
+if bot31:
+    bot31.start()
+    bot31.join_chat("spambotte")
+    
+if bot32:
+    bot32.start()
+    bot32.join_chat("spambotte")
+    
+if bot33:
+    bot33.start()
+    bot33.join_chat("spambotte")
+    
+if bot34:
+    bot34.start()
+    bot34.join_chat("spambotte")
+    
+if bot35:
+    bot35.start()
+    bot35.join_chat("spambotte")
+    
+if bot36:
+    bot36.start()
+    bot36.join_chat("spambotte")
+    
+if bot37:
+    bot37.start()
+    bot37.join_chat("spambotte")
+    
+if bot38:
+    bot38.start()
+    bot38.join_chat("spambotte")
+    
+if bot39:
+    bot39.start()
+    bot39.join_chat("spambotte")
+    
+if bot40:
+    bot40.start()
+    bot40.join_chat("spambotte")
+   
+if bot41:
+    bot41.start()
+    bot41.join_chat("spambotte")
+   
+if bot42:
+    bot42.start()
+    bot42.join_chat("spambotte")
+   
+if bot43:
+    bot43.start()
+    bot43.join_chat("spambotte")
+   
+if bot44:
+    bot44.start()
+    bot44.join_chat("spambotte")
+   
+if bot45:
+    bot45.start()
+    bot45.join_chat("spambotte")
+   
+if bot46:
+    bot46.start()
+    bot46.join_chat("spambotte")
+   
+if bot47:
+    bot47.start()
+    bot47.join_chat("spambotte")
+   
+if bot48:
+    bot48.start()
+    bot48.join_chat("spambotte")
+   
+if bot49:
+    bot49.start()
+    bot49.join_chat("spambotte")
+   
+if bot50:
+    bot50.start()
+    bot50.join_chat("spambotte")
+    
+print("Spam Bots Successfully Deployed ✅")
+print("Visit -! @phobiakalian")
+idle()
